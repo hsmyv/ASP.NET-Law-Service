91358dd1f669194c879bc9525ea609c4d9d687fe
Hasan <hsmusayev@gmail.com>
2023-07-01 13:11:50 +0400

    added migration

---
+++ Aspİntro/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aspİntro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aspİntro.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}

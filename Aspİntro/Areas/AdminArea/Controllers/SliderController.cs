683e0c9937d9bbdad47742feaf4ad2561b5f92b6
Hasan <hsmusayev@gmail.com>
2023-07-22 20:41:45 +0400

    update

--- Aspİntro/Areas/AdminArea/Controllers/SliderController.cs
+++ Aspİntro/Areas/AdminArea/Controllers/SliderController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Aspİntro.Data;
@@ -14,10 +15,12 @@ namespace Aspİntro.Areas.AdminArea.Controllers
     public class SliderController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvirment _env;
 
-        public SliderController(AppDbContext context)
+        public SliderController(AppDbContext context, IWebHostEnviroment env)
         {
             _context = context;
+            _env = env;
         }
         public async Task<IActionResult> Index()
         {
@@ -34,7 +37,28 @@ namespace Aspİntro.Areas.AdminArea.Controllers
         public async Task<IActionResult> Create(Slider slider)
         {
             if (ModelState["Photo"].ValidationState == ModelValidationState.Invalid) return View();
+            if (!slider.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "Image type is wrong");
+                return View();
+            }
+            if (slider.Photo.Length / 1024 > 100)
+            {
+                ModelState.AddModelError("Photo", "Image size is big than 100");
+                return View();
+            }
+            string filename = Guid.NewGuid().ToString() + "_" + slider.Photo.FileName;
+            string path = @"D:\Hasan\C# projects\AspNet\Aspİntro\Aspİntro\wwwroot\img\" + filename;
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                await slider.Photo.CopyToAsync(stream);
+            }
             return View();
         }
+    }
+
+    internal interface IWebHostEnvirment
+    {
     }
 }

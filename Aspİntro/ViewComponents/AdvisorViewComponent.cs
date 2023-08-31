f22189a803920b97e55850033b61a05a3343113e
Hasan <hsmusayev@gmail.com>
2023-08-31 13:51:19 +0400

    Update some pages

---
+++ Aspİntro/ViewComponents/AdvisorViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aspİntro.Data;
+using Aspİntro.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Aspİntro.ViewComponents
+{
+    public class AdvisorViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public AdvisorViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var advisors = await _context.Advisors.ToListAsync();
+            return (await Task.FromResult(View(advisors)));
+        }
+    }
+}
---
+++ Aspİntro/ViewComponents/FeatureViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aspİntro.Data;
+using Aspİntro.Models;
+using Aspİntro.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aspİntro.ViewComponents
+{
+    public class FeatureViewComponent: ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public FeatureViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Feature feature = await _context.Features.FirstOrDefaultAsync();
+
+            return (await Task.FromResult(View(feature)));
+        }
+    }
+}
